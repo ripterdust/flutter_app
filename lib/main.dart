77b35c27a561597eecab69d1aa39e2692f8aa33f
ripterdust <bryantello2010@hotmail.com>
2023-10-22 16:43:37 -0600

    Changed brightness mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:clinic_app/core/routers/app_router.dart';
 import 'package:clinic_app/core/themes/app_theme.dart';
 import 'package:clinic_app/core/providers/base_provider.dart';
-import 'package:clinic_app/screens/screens.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
